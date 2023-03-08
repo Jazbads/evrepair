507f7c2db881f42714fa8d079cc6cb5370dd98d8
Jazbads <jabzakung13@hotmail.com>
2023-03-09 06:33:03 +0700

    new

--- src/App.js
+++ src/App.js
@@ -1,15 +1,24 @@
 import "./App.css";
 import { BrowserRouter as Router, Switch, Route, Link, Routes } from "react-router-dom";
 import ErrorPage from "./error-page";
-import Sidebar from "./sidebar/sidebar";
-
+import Sidebarcashier from "./sidebar/sidebarcashier";
+import Sidebarcustomer from "./sidebar/sidebarcustomer";
+import Sidebaremployee from "./sidebar/sidebaremployee";
+import Sidebarowner from "./sidebar/sidebarowner";
+import Sidebarmanager from "./sidebar/sidebarmanager";
+import Sidebarboss from "./sidebar/sidebarboss";
 
 function App() {
   return (
     <div className="App">
       <Router>
         <Routes>
-          <Route path="/sidebar" element={<Sidebar />}/>
+          <Route path="/sidebarcashier" element={<Sidebarcashier />}/>
+          <Route path="/sidebarcustomer" element={<Sidebarcustomer />}/>
+          <Route path="/sidebaremployee" element={<Sidebaremployee />}/>
+          <Route path="/sidebarowner" element={<Sidebarowner />}/>
+          <Route path="/sidebarmanager" element={<Sidebarmanager />}/>
+          <Route path="/sidebarboss" element={<Sidebarboss />}/>
           <Route path="*" element={<ErrorPage />}/>
         </Routes>
       </Router>
